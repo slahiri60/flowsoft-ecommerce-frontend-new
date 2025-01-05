0a89a143069ab5a7a2168a927cbbad3a1eae895f
Soumitra Lahiri <soumitra.lahiri@gmail.com>
2025-01-04 21:35:05 -0800

    Configured Axios with base url and token

--- src/components/routes/PrivateRoute.js
+++ src/components/routes/PrivateRoute.js
@@ -12,14 +12,7 @@ export default function PrivateRoute() {
 
   useEffect(() => {
     const authCheck = async () => {
-      const { data } = await axios.get(
-        `${process.env.REACT_APP_API}/auth-check`,
-        {
-          headers: {
-            Authorization: auth?.token,
-          },
-        }
-      );
+      const { data } = await axios.get(`/auth-check`);
       if (data.ok) {
         setOk(true);
       } else {
