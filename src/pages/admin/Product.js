4b46a755946ddeedf8f66dfaf7823bd797a2b7f7
Soumitra Lahiri <soumitra.lahiri@gmail.com>
2025-01-08 07:42:57 -0800

    Implemented searchable category dropdown frontend

--- src/pages/admin/Product.js
+++ src/pages/admin/Product.js
@@ -1,10 +1,37 @@
+import { useState, useEffect } from 'react';
 import { useAuth } from '../../context/auth';
 import Jumbotron from '../../components/cards/Jumbotron';
 import AdminMenu from '../../components/nav/AdminMenu';
+import axios from 'axios';
+import { Select } from 'antd';
+
+const { Option } = Select;
 
 export default function AdminProduct() {
   // context
   const [auth, setAuth] = useAuth();
+  // state
+  const [categories, setCategories] = useState([]);
+  const [photo, setPhoto] = useState('');
+  const [name, setName] = useState('');
+  const [description, setDescription] = useState('');
+  const [price, setPrice] = useState('');
+  const [category, setCategory] = useState('');
+  const [shipping, setShipping] = useState('');
+  const [quantity, setQuantity] = useState('');
+
+  useEffect(() => {
+    loadCategories();
+  }, []);
+
+  const loadCategories = async () => {
+    try {
+      const { data } = await axios.get('/categories');
+      setCategories(data);
+    } catch (err) {
+      console.log(err);
+    }
+  };
 
   return (
     <>
@@ -21,7 +48,20 @@ export default function AdminProduct() {
           <div className="col-md-9">
             <div className="p-3 mt-2 mb-2 h4 bg-light">Create Products</div>
 
-            <p>Create products form...</p>
+            <Select
+              showSearch
+              bordered={false}
+              size="large"
+              className="form-select mb-3"
+              placeholder="Choose category"
+              onChange={(value) => console.log(value)}
+            >
+              {categories?.map((c) => (
+                <Option key={c._id} value={c.name}>
+                  {c.name}
+                </Option>
+              ))}
+            </Select>
           </div>
         </div>
       </div>
