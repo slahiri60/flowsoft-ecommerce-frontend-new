8e47c473e6629fcd11e0fe3c64d9092701ede4e0
Soumitra Lahiri <soumitra.lahiri@gmail.com>
2025-01-18 09:24:32 -0800

    Implemented product details page frontend

--- src/pages/admin/Product.js
+++ src/pages/admin/Product.js
@@ -3,62 +3,25 @@ import { useAuth } from '../../context/auth';
 import Jumbotron from '../../components/cards/Jumbotron';
 import AdminMenu from '../../components/nav/AdminMenu';
 import axios from 'axios';
-import { Select } from 'antd';
-import toast from 'react-hot-toast';
-import { useNavigate } from 'react-router-dom';
+import { Link } from 'react-router-dom';
+import moment from 'moment';
 
-const { Option } = Select;
-
-export default function AdminProduct() {
+export default function AdminProducts() {
   // context
   const [auth, setAuth] = useAuth();
   // state
-  const [categories, setCategories] = useState([]);
-  const [photo, setPhoto] = useState('');
-  const [name, setName] = useState('');
-  const [description, setDescription] = useState('');
-  const [price, setPrice] = useState('');
-  const [category, setCategory] = useState('');
-  const [shipping, setShipping] = useState('');
-  const [quantity, setQuantity] = useState('');
-  // hook
-  const navigate = useNavigate();
+  const [products, setProducts] = useState([]);
 
   useEffect(() => {
-    loadCategories();
+    loadProducts();
   }, []);
 
-  const loadCategories = async () => {
+  const loadProducts = async () => {
     try {
-      const { data } = await axios.get('/categories');
-      setCategories(data);
+      const { data } = await axios.get('/products');
+      setProducts(data);
     } catch (err) {
       console.log(err);
-    }
-  };
-
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    try {
-      const productData = new FormData();
-      productData.append('photo', photo);
-      productData.append('name', name);
-      productData.append('description', description);
-      productData.append('price', price);
-      productData.append('category', category);
-      productData.append('shipping', shipping);
-      productData.append('quantity', quantity);
-
-      const { data } = await axios.post('/product', productData);
-      if (data?.error) {
-        toast.error(data.error);
-      } else {
-        toast.success(`"${data.name}" is created`);
-        navigate('/dashboard/admin/products');
-      }
-    } catch (err) {
-      console.log(err);
-      toast.error('Product create failed. Try again.');
     }
   };
 
@@ -75,94 +38,42 @@ export default function AdminProduct() {
             <AdminMenu />
           </div>
           <div className="col-md-9">
-            <div className="p-3 mt-2 mb-2 h4 bg-light">Create Products</div>
+            <div className="p-3 mt-2 mb-2 h4 bg-light">Products</div>
 
-            {photo && (
-              <div className="text-center">
-                <img
-                  src={URL.createObjectURL(photo)}
-                  alt="product photo"
-                  className="img img-responsive"
-                  height="200px"
-                />
-              </div>
-            )}
+            {products?.map((p) => (
+              <Link
+                key={p._id}
+                to={`/dashboard/admin/product/update/${p.slug}`}
+              >
+                <div className="card mb-3">
+                  <div className="row g-0">
+                    <div className="col-md-4">
+                      <img
+                        src={`${process.env.REACT_APP_API}/product/photo/${p._id}`}
+                        alt={p.name}
+                        className="img img-fluid rounded-start"
+                      />
+                    </div>
 
-            <div className="pt-2">
-              <label className="btn btn-outline-secondary col-12 mb-3">
-                {photo ? photo.name : 'Upload photo'}
-                <input
-                  type="file"
-                  name="photo"
-                  accept="image/*"
-                  onChange={(e) => setPhoto(e.target.files[0])}
-                  hidden
-                />
-              </label>
-            </div>
-
-            <input
-              type="text"
-              className="form-control p-2 mb-3"
-              placeholder="Write a name"
-              value={name}
-              onChange={(e) => setName(e.target.value)}
-            />
-
-            <textarea
-              type="text"
-              className="form-control p-2 mb-3"
-              placeholder="Write a description"
-              value={description}
-              onChange={(e) => setDescription(e.target.value)}
-            />
-
-            <input
-              type="number"
-              className="form-control p-2 mb-3"
-              placeholder="Enter price"
-              value={price}
-              onChange={(e) => setPrice(e.target.value)}
-            />
-
-            <Select
-              // showSearch
-              bordered={false}
-              size="large"
-              className="form-select mb-3"
-              placeholder="Choose category"
-              onChange={(value) => setCategory(value)}
-            >
-              {categories?.map((c) => (
-                <Option key={c._id} value={c._id}>
-                  {c.name}
-                </Option>
-              ))}
-            </Select>
-
-            <Select
-              bordered={false}
-              size="large"
-              className="form-select mb-3"
-              placeholder="Choose shipping"
-              onChange={(value) => setShipping(value)}
-            >
-              <Option value="0">No</Option>
-              <Option value="1">Yes</Option>
-            </Select>
-
-            <input
-              type="number"
-              min="1"
-              className="form-control p-2 mb-3"
-              placeholder="Enter quantity"
-              value={quantity}
-              onChange={(e) => setQuantity(e.target.value)}
-            />
-
-            <button onClick={handleSubmit} className="btn btn-primary mb-5">
-              Submit
-            </button>
+                    <div className="col-md-8">
+                      <div className="card-body">
+                        <h5 className="card-title">{p?.name}</h5>
+                        <p className="card-text">
+                          {p?.description?.substring(0, 160)}...
+                        </p>
+                        <p className="card-text">
+                          <small className="text-muted">
+                            {moment(p.createdAt).format(
+                              'MMMM Do YYYY, h:mm:ss a'
+                            )}
+                          </small>
+                        </p>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </Link>
+            ))}
           </div>
         </div>
       </div>
