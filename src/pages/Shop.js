5bc093b3dc88d3056bef13e1d1a563851100c2fa
Soumitra Lahiri <soumitra.lahiri@gmail.com>
2025-01-17 17:42:43 -0800

    Implemented product filtering by category frontend

--- src/pages/Shop.js
+++ src/pages/Shop.js
@@ -2,10 +2,12 @@ import { useState, useEffect } from 'react';
 import Jumbotron from '../components/cards/Jumbotron';
 import axios from 'axios';
 import ProductCard from '../components/cards/ProductCard';
+import { Checkbox } from 'antd';
 
 export default function Shop() {
   const [categories, setCategories] = useState([]);
   const [products, setProducts] = useState([]);
+  const [checked, setChecked] = useState([]); // categories
 
   useEffect(() => {
     loadProducts();
@@ -33,13 +35,40 @@ export default function Shop() {
     }
   };
 
+  const handleCheck = (value, id) => {
+    console.log(value, id);
+    let all = [...checked];
+    if (value) {
+      all.push(id);
+    } else {
+      all = all.filter((c) => c !== id);
+    }
+    setChecked(all);
+  };
+
   return (
     <>
       <Jumbotron title="Hello World" subTitle="Welcome to React E-commerce" />
 
+      <pre>{JSON.stringify({ checked }, null, 4)}</pre>
+
       <div className="container-fluid">
         <div className="row">
-          <div className="col-md-3">sidebar</div>
+          <div className="col-md-3">
+            <h2 className="p-3 mt-2 mb-2 h4 bg-light text-center">
+              Filter by Categories
+            </h2>
+            <div className="row p-5">
+              {categories?.map((c) => (
+                <Checkbox
+                  key={c._id}
+                  onChange={(e) => handleCheck(e.target.checked, c._id)}
+                >
+                  {c.name}
+                </Checkbox>
+              ))}
+            </div>
+          </div>
 
           <div className="col-md-9">
             <h2 className="p-3 mt-2 mb-2 h4 bg-light text-center">
